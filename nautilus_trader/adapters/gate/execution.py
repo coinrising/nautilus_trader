9d6f340c0d3efaa4d89d272ed298b278430ebd7e
Mona Lisa <YOUR_EMAIL>
2025-07-11 12:37:37 +0800

    debug3

--- nautilus_trader/adapters/gate/execution.py
+++ nautilus_trader/adapters/gate/execution.py
@@ -144,8 +144,8 @@ class GateExecutionClient(LiveExecutionClient):
         self._retry_manager_pool = RetryManagerPool[None](
             pool_size=100,
             max_retries=config.max_retries or 0,
-            delay_initial_ms=config.retry_delay_initial_ms or 1_000,
-            delay_max_ms=config.retry_delay_max_ms or 10_000,
+            delay_initial_ms=config.delay_initial_ms or 1_000,
+            delay_max_ms=config.delay_max_ms or 10_000,
             backoff_factor=2,
             logger=self._log,
             exc_types=(GateError,),
